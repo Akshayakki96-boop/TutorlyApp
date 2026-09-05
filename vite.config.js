040ecb71ca1648371da68842085afedcfaa515dd
Santosh Peddi <santosh.peddi@championx.com>
2026-09-05 12:26:28 +0530

    feat: add server configuration to specify port in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: 'dist',
     rollupOptions: {
